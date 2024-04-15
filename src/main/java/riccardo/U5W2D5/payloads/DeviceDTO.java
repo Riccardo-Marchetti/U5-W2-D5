fb8b07d1146cc257fba1c7f3ebd5eee79e0145b6
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-04-15 11:28:27 +0200

    .

--- src/main/java/riccardo/U5W2D5/payloads/DeviceDTO.java
+++ src/main/java/riccardo/U5W2D5/payloads/DeviceDTO.java
@@ -7,6 +7,5 @@ import java.util.UUID;
 public record DeviceDTO(
                         @NotEmpty(message = "Il tipo è obbligatorio")
                         @Pattern (regexp = "Smartphone|Tablet|Laptop|smartphone|tablet|laptop", message = "Inserisci un tipo valido tra Smartphone, Tablet, Laptop")
-                        @Size (min = 1 , max = 20, message = "Il tipo deve avere la lunghezza dei caratteri compresa tra 1 e 20")
                         String type) {
 }
